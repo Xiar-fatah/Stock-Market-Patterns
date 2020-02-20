f8c2384a9de70587ebffc3274f49bcc12ffc70ce
Xiar-fatah <54110012+Xiar-fatah@users.noreply.github.com>
2020-02-20 18:57:51 +0100

    Update Indicators.py

--- Two_Features/Indicators.py
+++ Two_Features/Indicators.py
@@ -5,6 +5,7 @@ import pandas as pd
 url = 'https://raw.githubusercontent.com/mwitiderrick/stockprice/master/NSE-TATAGLOBAL.csv'
 read_in_dataset= pd.read_csv(url)
 
+# The open, high .. etc needs to be numpy arrays for tulipy
 data_open = np.asarray(read_in_dataset.iloc[:,1].tolist())
 data_high = np.asarray(read_in_dataset.iloc[:,2].tolist())
 data_low = np.asarray(read_in_dataset.iloc[:,3].tolist())
@@ -13,10 +14,15 @@ data_closing = np.asarray(read_in_dataset.iloc[:,4].tolist())
 # Simple moving average for periods 5, 10 and 20.
 ma5,ma10,ma20 = ti.sma(data_open,5), ti.sma(data_open,10), ti.sma(data_open,20)
 
-# diff = EMA12-EMA26 
+# diff = EMA12-EMA26, also known as the MACD indicator
 diff = ti.ema(data_closing, 12) - ti.ema(data_closing,26)
 
-# RSI, Relative Strong Index
+# RSI, Relative Strong Index for periods 6 and 12
+rsi6 = ti.rsi(data_closing,6)
+rsi12= ti.rsi(data_closing,12)
+
+# willr, Williams Index for periods 5 and 10
+willr_5,willr_10 = ti.willr(data_high,data_low,data_closing,5), ti.willr(data_high,data_low,data_closing,10)
 
 
 #def print_info(indicator):
