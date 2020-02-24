d5e57274845df5059954e02b8be2365736a246c5
Xiar-fatah <54110012+Xiar-fatah@users.noreply.github.com>
2020-02-24 23:21:10 +0100

    Update test.py

--- Multiple_Features/test.py
+++ Multiple_Features/test.py
@@ -15,21 +15,21 @@ ti = TechIndicators(key, output_format='pandas')
 MSFT_data, _ = ts.get_daily(symbol='MSFT', outputsize = 'full')
 MSFT_data = MSFT_data[1:]
 # aapl_sma is a dict, aapl_meta_sma also a dict
-#MSFT_sma5, _ = ti.get_sma(symbol='MSFT',time_period='5')
-#MSFT_sma10, _ = ti.get_sma(symbol='MSFT',time_period='10')
-#MSFT_sma20, _ = ti.get_sma(symbol='MSFT',time_period='20')
-#MSFT_mom6, _ = ti.get_mom(symbol='MSFT',time_period='6')
-#time.sleep(61)
-#MSFT_mom12, _ = ti.get_mom(symbol='MSFT',time_period='12')
-#MSFT_ultosc, _ = ti.get_ultosc(symbol='MSFT')
-##MSFT_ema, _ = ti.get_ema(symbol='MSFT')
-#MSFT_macd, _ = ti.get_macd(symbol='MSFT')
-#MSFT_rsi6, _ = ti.get_rsi(symbol='MSFT',time_period='6')
-#time.sleep(61)
-#MSFT_rsi12, _ = ti.get_rsi(symbol='MSFT',time_period='12')
-#MSFT_willr, _ = ti.get_willr(symbol='MSFT')
-#MSFT_roc, _ = ti.get_roc(symbol='MSFT')
-#MSFT_stoch,_ = ti.get_stoch(symbol='MSFT')
+MSFT_sma5, _ = ti.get_sma(symbol='MSFT',time_period='5')
+MSFT_sma10, _ = ti.get_sma(symbol='MSFT',time_period='10')
+MSFT_sma20, _ = ti.get_sma(symbol='MSFT',time_period='20')
+MSFT_mom6, _ = ti.get_mom(symbol='MSFT',time_period='6')
+time.sleep(61)
+MSFT_mom12, _ = ti.get_mom(symbol='MSFT',time_period='12')
+MSFT_ultosc, _ = ti.get_ultosc(symbol='MSFT')
+#MSFT_ema, _ = ti.get_ema(symbol='MSFT')
+MSFT_macd, _ = ti.get_macd(symbol='MSFT')
+MSFT_rsi6, _ = ti.get_rsi(symbol='MSFT',time_period='6')
+time.sleep(61)
+MSFT_rsi12, _ = ti.get_rsi(symbol='MSFT',time_period='12')
+MSFT_willr, _ = ti.get_willr(symbol='MSFT')
+MSFT_roc, _ = ti.get_roc(symbol='MSFT')
+MSFT_stoch,_ = ti.get_stoch(symbol='MSFT')
 
 MSFT_data['SMA5'] = MSFT_sma5
 MSFT_data['SMA10'] = MSFT_sma10
@@ -42,9 +42,10 @@ MSFT_data['RSI6'] = MSFT_rsi6
 MSFT_data['RSI12'] = MSFT_rsi12
 MSFT_data['WILLR'] = MSFT_willr
 MSFT_data['ROC'] = MSFT_roc
+# TODO: Find the lowest val
 MSFT_data = MSFT_data[:5004]
 MSFT_data.to_csv("fin.csv")
-
+# TODO: FIX THESE INDICATORS
 #MSFT_data['D'] = MSFT_stoch_D
 #MSFT_data['K'] = MSFT_stoch_K
 
